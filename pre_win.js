f3e09a08f4e4021ff61c7f9be03b7a17f315929b
braagaa <michael_braga@outlook.com>
2024-06-08 20:32:12 -0400

    feat(webauthn helper): added a static helper class for WebAuthn API calls

--- pre_win.js
+++ pre_win.js
@@ -1,28 +1,28 @@
-import * as fs from "fs";
-import * as yaml from "js-yaml";
+import * as fs from 'fs'
+import * as yaml from 'js-yaml'
 
-const file = fs.readFileSync("./openapi.yaml", "utf8");
-const data = yaml.load(file);
+const file = fs.readFileSync('./openapi.yaml', 'utf8')
+const data = yaml.load(file)
 
 for (const pathKey in data.paths) {
-  const path = data.paths[pathKey];
+  const path = data.paths[pathKey]
 
   for (const methodKey in path) {
-    const method = path[methodKey];
+    const method = path[methodKey]
     if (method.security?.some((sec) => sec.jwt_header_Authorization)) {
       if (!method.parameters) {
-        method.parameters = [];
+        method.parameters = []
       }
 
       method.parameters.push({
-        name: "Authorization",
-        in: "header",
-        description: "JWT Authorization header",
+        name: 'Authorization',
+        in: 'header',
+        description: 'JWT Authorization header',
         required: false,
-        schema: { type: "string" },
-      });
+        schema: { type: 'string' },
+      })
     }
   }
 }
 
-fs.writeFileSync("./mod-openapi.yaml", yaml.dump(data), "utf8");
+fs.writeFileSync('./mod-openapi.yaml', yaml.dump(data), 'utf8')
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import LoginIDWebSDK from './loginid'
 import PasskeyError from './errors/passkey'
+import {WebAuthnHelper} from './webauthn/webauthn-helper'
 import {createPasskeyCredential, getPasskeyCredential} from './webauthn'
 import type {DoesDeviceSupportPasskeysResponse} from './browser'
 import {doesDeviceSupportPasskeys, isConditionalUIAvailable, isPlatformAuthenticatorAvailable} from './browser'
@@ -12,9 +13,11 @@ export {
   isPlatformAuthenticatorAvailable,
   LoginIDWebSDK,
   PasskeyError,
+  WebAuthnHelper
 }
 export type {DoesDeviceSupportPasskeysResponse}
 
 export * from './loginid/types'
+export * from './api'
 
 export default LoginIDWebSDK
